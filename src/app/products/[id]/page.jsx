ebad3c38bf9f9c47c463445e1c0f08d2bdb3ffb2
Asmual <Asmual04@gmail.com>
2026-05-02 01:02:18 +0600

    Products Page Complete-1

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -3,7 +3,7 @@ import ProductDetails from "@/components/ProductDetails";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default async function Page({ params }) {
-  const { id } = await params; // ✅ Next.js 15 fix
+  const { id } = await params;
 
   const product = products.find(
     (item) => item.id === Number(id)
